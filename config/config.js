ffaf742c19ad81ef6042a6c241735bbad1d9704a
M-Ayers <47603709+M-Ayers@users.noreply.github.com>
2020-12-05 00:07:31 -0600

    feat(feature-toggles): adding feature toggles

--- config/config.js
+++ config/config.js
@@ -2,9 +2,11 @@ const {
   HUE_IP_ADDR,
   HUE_API_KEY,
   MQTT_IP_ADDR,
+  TOGGLE_TYPE,
   UNLEASH_IP_ADDR,
   UNLEASH_INSTANCE_ID,
   PORT,
+
 } = process.env;
 
 module.exports = {
@@ -13,4 +15,5 @@ module.exports = {
   mqttUrl: `mqtt://${MQTT_IP_ADDR}:1883`,
   unleashUrl: `http://${UNLEASH_IP_ADDR}:4242`,
   unleashInstanceId: UNLEASH_INSTANCE_ID,
+  toggleType: TOGGLE_TYPE,
 };
